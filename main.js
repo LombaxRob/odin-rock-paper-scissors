22d142cf332c2ef37c8fc676e2e889728a71260c
Robert Borland <50889172+LombaxRob@users.noreply.github.com>
2025-08-05 17:24:43 +0100

    feat: create getComputerChoice and test
    Created the getComputerChoice function and tested
    it by outputting the value, fixed 2 logic errors.
    First was that it was not whole numbers, used
    Math.floor to fix and second was the incorrect
    range, increased range by 1.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function compChoice (){
+    let choiceNum = Math.floor((Math.random() * 3));
+    console.log(choiceNum)
+    if (choiceNum == 0) {
+        return "Rock";
+    } else if (choiceNum == 1) {
+        return "Paper";
+    } else if (choiceNum == 2) {
+        return "Scissors";
+    } else {
+        return "Error: compChoice function not returning the correct value";
+    }
+}
+
+console.log(compChoice());
